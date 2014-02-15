b5a549474777f975fe5b66017e64a63ff02090bd
ykawasak <akiu.bullet@gmail.com>
2014-02-15 17:11:38 +0900

    とりあえず見てSayが押せるようになった。だめだったらこれで行く

--- Android/UlsayApps/src/com/kabutomushi/ulsayapps/NewsCardAdapter.java
+++ Android/UlsayApps/src/com/kabutomushi/ulsayapps/NewsCardAdapter.java
@@ -8,28 +8,32 @@ import android.widget.TextView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class NewsCardAdapter extends ArrayAdapter<NewsCardData> {
 	private LayoutInflater layoutInflater;
+	private int mLastAnimationPosition = 0;
+	private Context mContext;
 
 	public NewsCardAdapter(Context context, int textViewResourceId,
 			List<NewsCardData> objects) {
 		super(context, textViewResourceId, objects);
 		layoutInflater = (LayoutInflater) context
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		mContext = context;
+		
 	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		// 特定の行(position)のデータを得る
 		NewsCardData item = (NewsCardData) getItem(position);
 
-		// convertViewは使い回しされている可能性があるのでnullの時だけ新しく作る
 		if (null == convertView) {
 			convertView = layoutInflater.inflate(R.layout.news_card, null);
 		}
 
-		// CustomDataのデータをViewの各Widgetにセットする
+		//データ入れる
 		ImageView imageView;
 		imageView = (ImageView) convertView.findViewById(R.id.newsISmage);
 		imageView.setImageBitmap(item.getImg());
@@ -37,6 +41,14 @@ public class NewsCardAdapter extends ArrayAdapter<NewsCardData> {
 		TextView textView;
 		textView = (TextView) convertView.findViewById(R.id.newsTitle);
 		textView.setText(item.getTitle());
+
+		// アニメーションを設定
+		if (mLastAnimationPosition < position) {
+			Animation animation = AnimationUtils.loadAnimation(mContext,
+					R.anim.motion);
+			convertView.startAnimation(animation);
+			mLastAnimationPosition = position;
+		}
 
 		return convertView;
 	}
--- Android/UlsayApps/src/com/kabutomushi/ulsayapps/UlsayActivity.java
+++ Android/UlsayApps/src/com/kabutomushi/ulsayapps/UlsayActivity.java
@@ -8,13 +8,23 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.Window;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterViewFlipper;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
 public class UlsayActivity extends Activity {
+	
+	int mCardId;
+	private Button mSayButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,8 @@ public class UlsayActivity extends Activity {
 		setContentView(R.layout.activity_ulsay);
 		
 		final ListView contentView = (ListView)findViewById(R.id.content);
+		mSayButton = (Button)findViewById(R.id.sayButton);
+		mSayButton.setVisibility(View.INVISIBLE);
 		
 		//ニュースデータ作る(今回は仮)
 		Bitmap img;
@@ -46,9 +58,52 @@ public class UlsayActivity extends Activity {
 		item2.setImg(img);
 		newsData.add(item2);
 		
+		NewsCardData item3 = new NewsCardData();
+		item3.setTitle("いいえ、ためふさのせいです。");
+		item3.setImg(img);
+		newsData.add(item3);
+		
+		NewsCardData item4 = new NewsCardData();
+		item4.setTitle("川崎結花氏による東京ドーム年越しライブ");
+		item4.setImg(img);
+		newsData.add(item4);
+		
+		NewsCardData item5 = new NewsCardData();
+		item5.setTitle("浦添さん あまりのメガホン音量に激怒");
+		item5.setImg(img);
+		newsData.add(item5);
 		//リストにアダプターをセット
 		NewsCardAdapter adapter = new NewsCardAdapter(this, 0, newsData);
 		contentView.setAdapter(adapter);
+				
+	
+		//記事タップでSayボタン表示
+		contentView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View view, int position,
+					long id) {
+				Log.d("ulsay", "num = " + position);
+				mCardId = position;
+				mSayButton.setVisibility(View.VISIBLE);
+			}
+			
+		});
+		
+
+		contentView.setOnScrollListener(new OnScrollListener() {
+
+			@Override
+			public void onScroll(AbsListView arg0, int arg1, int arg2, int arg3) {
+				mSayButton.setVisibility(View.INVISIBLE);
+			}
+
+			@Override
+			public void onScrollStateChanged(AbsListView arg0, int arg1) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 		
 	}
 
@@ -59,7 +114,9 @@ public class UlsayActivity extends Activity {
 		return true;
 	}
 	public void onClickSayButton (View view){
-		 Toast.makeText(this, "SAY!!!!!", Toast.LENGTH_LONG).show();
+		 Toast.makeText(this, "SAY!!!!!num=" + mCardId, Toast.LENGTH_LONG).show();
+		mSayButton.setVisibility(View.INVISIBLE);
 	}
+	
 
 }
